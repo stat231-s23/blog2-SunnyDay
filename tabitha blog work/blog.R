4f0c7eaf43765601655bf26fe93455b39627da88
Tabitha <tatus22t@mtholyoke.edu>
2023-04-26 16:45:22 -0400

    yay working graphs

--- tabitha blog work/blog.R
+++ tabitha blog work/blog.R
@@ -26,9 +26,33 @@ pubs_data <- pubs_info %>%
 
 #create publication prevalence by country barchart
 country_pubs <- pubs_data %>%
-  separate(country_of_research_organization
-           , into=c('Country 1', 'Country 2', 'Country 3', 'Country 4')
-           , sep = ";")
+  rename(country = country_of_research_organization) %>%
+  unnest_tokens(output = country, input = country, 
+                token = 'regex', pattern=";", to_lower = F) %>%
+  mutate(across(where(is.character), str_trim)) %>%
+  #this removes duplicate countries for one publication
+  #ex: first title had 5 Kuwait authors, but still only 1 pub coming from Kuwait
+  group_by(country) %>%
+  filter(title!=lag(title) | is.na(lag(title))) %>%
+  group_by(pub_year) %>%
+  count(country, sort = T)
+
+country_pubs %>%
+  slice(1:10) %>%
+  ggplot(aes(x = fct_reorder(country, n), y = n, color = country, fill = country)) +
+  geom_col() +
+  geom_label(aes(label = n), color = "black", fill = "white", 
+             label.padding = unit(0.1, "lines"), label.size = NA) +
+  facet_grid(. ~ pub_year) +
+  # Rotate graph
+  coord_flip() +
+  guides(color = "none", 
+         fill = "none") +
+  labs(
+    title = "Top Publication Producing Countries in 2020 and 2021",
+    x = "Publication Count",
+    y = "Country"
+  )
   
 
 
