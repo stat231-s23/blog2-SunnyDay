bdcba20b81658daa61599d51a4691367415909c7
lkaminka <lkaminka25@amherst.edu>
2023-04-27 21:54:55 -0400

    9:54

--- Lynca blog work/app.R
+++ Lynca blog work/app.R
@@ -9,8 +9,6 @@ library(xts)
 library(shinythemes)
 
 #import data
-
-
 covid_worldwide_obs <- readRDS("data/covid_worldwide_7_leaflet.Rds")
 
 
@@ -20,14 +18,46 @@ library(leaflet)
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
 
-my_palette <- colorNumeric ( palette = "YIGnBu", 
-                             domain = covid_worldwide_obs$people_vaccinated)
 
-ui <- fluidPage(
-  leafletOutput("mymap"),
-  p(), #adds a line break for aesthetic reasons.
-  actionButton("recalc", "New points")
+#restricting the analysis to a choice of continet: 
+continent_choices <- unique(covid_worldwide_obs$continent)
+
+my_palette <- colorNumeric ( palette = "YIGnBu", 
+                             domain = covid_worldwide_obs$people_vaccinated, 
+                             alpha = 0.8)
+
+ui <- fluidPage (
+title="Comparative Analysis of Covid-19 Statistics and Wealth Indicators Across Different Countries.",
+tabPanel (
+  title = "Choose one indicator of a country's wealth.",
+  sidebarLayout(
+    sidebarPanel(
+
+    ),
+    
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel( "Map Summary", 
+        withSpinner(leafletOutput("mymap")), 
+        p(), #adds a line break for aesthetic reasons.
+        actionButton("recalc", "New points")),
+        
+        tabPanel ( "PlotSummary", 
+                   withSpinner(plotOutput(outputId = "linegraph"))
+                   
+                   
+          
+        )
+        
+      )
+    )
+  )
 )
+)
+
+
+
 
 server <- function(input, output, session) {
   
@@ -41,9 +71,11 @@ server <- function(input, output, session) {
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
       addPolygons (
-        fillColor = ~my_palette(people_vaccinated), 
+        fillColor = ~my_palette(covid_worldwide_obs$people_vaccinated),
+        color = "#b2aeae", 
         stroke = FALSE, 
-        popup = ~ glue:: glue("Country_name: {str_to_title(name)} <br>"))
+        popup = ~ glue:: glue("Country_name: {str_to_title(name)} <br>")) %>%
+      addMarkers ( data = covid_worldwide_obs, lat = ~latitude, lng = ~longitude)
   })
 }
 
