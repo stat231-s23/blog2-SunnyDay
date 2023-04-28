d6db2346b5df000f20f31c2cfd7af529ecdb3dc5
lkaminka <lkaminka25@amherst.edu>
2023-04-28 00:00:38 -0400

    12:00

--- Lynca blog work/app.R
+++ Lynca blog work/app.R
@@ -22,9 +22,13 @@ names(r_colors) <- colors()
 #restricting the analysis to a choice of continet: 
 continent_choices <- unique(covid_worldwide_obs$continent)
 
-my_palette <- colorNumeric ( palette = "YIGnBu", 
-                             domain = covid_worldwide_obs$people_vaccinated, 
-                             alpha = 0.8)
+
+
+#gathering some observations: 
+poverty_indicator_names <- c("Population Density (persons per km²)", "Extreme Poverty ($per day)", "Diabetes Prevalence")
+poverty_indicator_values <- c("population_density", "extreme_poverty", "diabetes_prevalence")
+
+names(poverty_indicator_values) <- poverty_indicator_names
 
 ui <- fluidPage (
 title="Comparative Analysis of Covid-19 Statistics and Wealth Indicators Across Different Countries.",
@@ -32,7 +36,17 @@ tabPanel (
   title = "Choose one indicator of a country's wealth.",
   sidebarLayout(
     sidebarPanel(
-
+      
+      selectInput(inputId = "id_continent_choices"
+                     , label = "Identify which continents you want to focus your analysis on:"
+                     , choices = continent_choices
+                     , selected = NULL
+                     , multiple = TRUE),
+    
+    selectInput ( inputId = "poverty_Indicator", 
+                  label = "Choose an indicator of poverty", 
+                  choices = poverty_indicator_values, 
+                  selected = NULL)
     ),
     
     
@@ -46,8 +60,7 @@ tabPanel (
         tabPanel ( "PlotSummary", 
                    withSpinner(plotOutput(outputId = "linegraph"))
                    
-                   
-          
+                  
         )
         
       )
@@ -56,27 +69,77 @@ tabPanel (
 )
 )
 
-
-
-
+  
+  #selecting the right data to display, and making it reactive to the user's input.
+  
 server <- function(input, output, session) {
+
+  mydata <- reactive (
+    {
+      return (filter(covid_worldwide_obs, continent %in% input$id_continent_choices))
+    }
+  )
   
   points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+   # cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
   }, ignoreNULL = FALSE)
   
   output$mymap <- renderLeaflet({
+    
+    my_palette <- colorNumeric(
+      palette = "YlGnBu", 
+      domain = covid_worldwide_obs[[input$poverty_Indicator]]
+    )
+    
     leaflet(data = covid_worldwide_obs) %>%
       addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
+                      options = providerTileOptions(noWrap = TRUE)
       ) %>%
-      addPolygons (
-        fillColor = ~my_palette(covid_worldwide_obs$people_vaccinated),
+      
+     addPolygons (
+        fillColor = ~my_palette(covid_worldwide_obs[[input$poverty_Indicator]]),
         color = "#b2aeae", 
         stroke = FALSE, 
-        popup = ~ glue:: glue("Country_name: {str_to_title(name)} <br>")) %>%
-      addMarkers ( data = covid_worldwide_obs, lat = ~latitude, lng = ~longitude)
+        weight = 1, 
+        fillOpacity = 0, 
+        popup = ~ glue:: glue("Country_name: {str_to_title(name)} <br>"))
+      
+  
+      
+      #addMarkers ( data = covid_worldwide_obs, lat = ~latitude, lng = ~longitude)
   })
 }
+
+#server <- function(input, output, session) {
+  
+ # points <- eventReactive(input$recalc, {
+   
+ # }, ignoreNULL = FALSE)
+  
+  #output$mymap <- renderLeaflet({
+    
+    #covid_worldwide_obs_transformed <- st_transform(covid_worldwide_obs, 4326)
+    
+   # my_palette <- colorNumeric(
+    #  palette = "YlGnBu", 
+    #  domain = covid_worldwide_obs_transformed[[input$poverty_Indicator]]
+   # )
+    
+   # leaflet(data = covid_worldwide_obs_transformed) %>%
+    #  addProviderTiles(providers$Stamen.TonerLite,
+     #                  options = providerTileOptions(noWrap = TRUE)
+   #   ) %>%
+   #   addPolygons (
+    #    fillColor = ~my_palette(covid_worldwide_obs_transformed[[input$poverty_Indicator]]),
+    #    color = "#b2aeae", 
+    #    stroke = FALSE, 
+    #    popup = ~ glue:: glue("Country_name: {str_to_title(name)} <br>") %>%
+    # addLegend(pal = my_palette,
+    #          values = covid_worldwide_obs_transformed[[input$poverty_Indicator]],
+     #         position = "bottomright",
+     #         title = poverty_indicator_names[poverty_indicator_values == input$poverty_Indicator]))
+      #addMarkers ( data = covid_worldwide_obs, lat = ~latitude, lng = ~longitude)
+ # })
+#}
 
 shinyApp(ui, server)
